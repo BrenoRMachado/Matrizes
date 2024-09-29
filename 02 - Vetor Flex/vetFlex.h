2320a15f5160312c525ec6b73dc22232fb104096
Bereno <burracha.6464@gmail.com>
2024-09-28 21:45:45 -0300

    define a classe

--- 02 - Vetor Flex/vetFlex.h
+++ 02 - Vetor Flex/vetFlex.h
@@ -1,4 +1,19 @@
 class VetFlex 
 {
+    private:
+        int n; //tamanho do vetor
+        float* vet; //vai permitir alocar um vetor
+        int start, end; // vai nreceber o indici inicial e o final
+
+        int detind(int indice);//converte o indice (intervalo) para indice (padrão)
+
+    public:
+
+        VetFlex(int a, int b);
+        ~VetFlex();
+
+        float get(int indice);
+        void set(int indice, float valor);
+        
 
 };
