8f28b719ba20446e3b750a17fefdc910321bb758
Bereno <burracha.6464@gmail.com>
2024-09-29 11:21:18 -0300

    cria a main

--- 02 - Vetor Flex/main.cpp
+++ 02 - Vetor Flex/main.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main ()
 {
+    int a = -33, b = 30;
+
+    VetFlex vet1(a,b);
+
+    for(int i = a; i < b ; i++)
+    {
+        double val = (i - a) + 1;
+        vet1.set(i,val);
+    }
+
+    for(int i = a; i<b ; i++)
+    {
+        double val = vet1.get(i);
+        cout << val <<endl;
+    }
 
     return 0;
 }
