718b2dd0ba2da49304fd496d9fd82cf6f7f1b444
Bereno <burracha.6464@gmail.com>
2024-09-29 23:12:31 -0300

    criação do construtor e destrutor

--- 03 - Matriz comum/matriz.cpp
+++ 03 - Matriz comum/matriz.cpp
@@ -1,3 +1,42 @@
 #include <iostream>
 #include "matriz.h"
 using namespace std;
+
+
+Matriz :: Matriz(int tamp, int tamf)
+{
+    //associação das variáveis da classe com os parÂmetros
+    p = tamp; // tamanho do vetor de ponteiros
+    f = tamf; // tamanho dos vetores filhos
+
+    //criação do vetor de ponteiros
+    // int ** mat = new int*[tamp];  ERRADO, para de inicializar no construtor
+    // o problema é que esse mat só vai existir nesse escopo, então cria no matriz.h
+
+    mat = new float*[p]; //criação de um vetor de ponteiros que apontam para vetores
+
+    // criação do vetor de cada ponteiro
+    for (int i = 0 ; i < p ; i++)
+    {
+        mat[i] = new float[f]; //criação dos vetores filhos
+    }
+
+    //inicialização dos vetores filhos
+
+    for(int i = 0 ; i < p ; i++)
+    {
+        for(int j = 0 ; j < f ; j++)
+        {
+            mat[i][j] = 0;
+        }
+    }
+}
+
+Matriz :: ~Matriz()
+{
+    for(int i = 0 ; i< p ; i++)
+    {
+        delete [] mat[i];
+    }
+    delete [] mat;
+}
--- 03 - Matriz comum/matriz.h
+++ 03 - Matriz comum/matriz.h
@@ -2,6 +2,23 @@ class Matriz
 {
     private:
 
+    int p = 0 , f = 0;
+    // p = tamanho do vetor de ponteiros
+    // f = tamanho dos vetores filhos
+
+    float ** mat; //ponteiro para o vetor de ponteiros
+
     public:
-    
+
+    // Construtores e destrutor ------------------------------------------
+
+    Matriz(int tamp, int tamf);
+    ~Matriz();
+
+    // tamp = Tamanho do vetor de ponteiros
+    // tamf = tamanho dos vetores filhos
+
+    // -------------------------------------------------------------------
+
+
 };
