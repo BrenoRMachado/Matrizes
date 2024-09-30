d8cf564acc077bca24330f764006466fd1f84fd0
Bereno <burracha.6464@gmail.com>
2024-09-30 17:37:53 -0300

    classe

--- B01 - Matriz Linha/matriz.h
+++ B01 - Matriz Linha/matriz.h
@@ -1,7 +1,21 @@
 class MatrizLin
 {
     private:
+    int mm, nn;
+    float *vet; //vetor vai ter o tamanho m*n
+    
+    int detInd(int m, int n);
+    // m  = linhas
+    // n = colunas
 
     public:
+    MatrizLin(int m, int n);
+    // m = numero de linhas
+    // n = numero de colunas
+    ~MatrizLin();
 
+    float get(int i, int j);
+    // i = indice linha
+    // j = indice coluna
+    void set(int i, int j, float valor);
 };
